a8b9be67fa46d8949e45817ada232edf5b15e99e
sparse-14 <pshrey964@gmail.com>
2023-10-31 21:31:03 +0530

    4th update

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-echo "BUILD START"
- C:/Users/Shrey Patel/AppData/Local/Programs/Python/Python311 -m pip install -r requirements.txt
- C:/Users/Shrey Patel/AppData/Local/Programs/Python/Python311 manage.py collectstatic --noinput --clear
+ echo "BUILD START"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
